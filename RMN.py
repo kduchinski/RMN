2bfc0c1514806d5ea81b94b36a99827d231ecc33
kduchinski <kduchinski@gmail.com>
2019-04-02 14:56:50 -0400

    Working version

--- RMN.py
+++ RMN.py
@@ -32,17 +32,18 @@ import sys
 # In[3]:
 
 
-def format_file(file, oligo = False):
+def format_file(file, oligo = "False"):
     print("start format_file")
-    if(oligo in ["true", "True", "T", "TRUE", "1"]):
+    if(oligo in ["true", "True", "T", "1"]):
         df = format_oligo(file)
     else:
-        df = pd.read_table(file, sep = "\t")
-        df = df.iloc[:,3:].transpose()
-        df.insert(0, "OTU", list(df.index))
+        df = pd.read_table(file, sep = ",")
+        #df = df.iloc[:50,3:].transpose()
+        #df.insert(0, "OTU", list(df.index))
         df.index = range(0, len(df.index))
-        df.iloc[:,1:] = df.iloc[:,1:]/100
+        #df.iloc[:,1:] = df.iloc[:,1:]/100
     print("end format_file")
+    print(df)
     return df
 
 # In[4]:
@@ -54,10 +55,12 @@ def format_oligo(file):
     if("numOtus" in df.columns):
         df = df.drop(["numOtus"], axis = 1)
     df = df.iloc[:,1:].transpose()
+    #df.apply(pd.to_numeric, errors = 'ignore')
     df.insert(0, "Oligotype", list(df.index))
     df.index = range(0, len(df.index))
     df.iloc[:,1:] = df.iloc[:,1:]/100
     print("end format_oligo")
+
     return df
 
 # In[7]:
@@ -68,8 +71,8 @@ def pairs(df):
     p = p.rename({0: "Om", 1: "Oc"}, axis = "columns")
     return p
 def triplets(df):
-    t = pd.DataFrame(list(permutations(list(df.index), r=2)))
-    t = t.rename({0: "Om", 1: "Oc"}, axis = "columns")
+    t = pd.DataFrame(list(permutations(list(df.index), r=3)))
+    t = t.rename({0: "Om", 1: "Oc", 2: "Ot"}, axis = "columns")
     return t
 
 
@@ -122,22 +125,24 @@ def predict_SRP(df):
 def lack_of_fit(df, triplet_df, skip = -1):
     print("start lack_of_fit")
     exp = predict_SRP(df)
-    obs = obs_SRP_df = df.iloc[list(triplet_df["Ot"]),1:].reset_index(drop=True)
+    obs = df.iloc[list(triplet_df["Ot"]),1:].reset_index(drop=True)
     
     if(skip != -1):
         df = df.drop(df.columns[skip], axis=1)
         obs = obs.drop(obs.columns[skip-2], axis=1)
         exp = exp.drop(exp.columns[skip-2], axis=1)
-        
+
     errors = list()
     for i in range(0,len(exp.columns)):
         errors.append(list(obs.iloc[:,i] - exp.iloc[:,i]))
     sse_df = pd.DataFrame(errors).transpose()**2
     sse_df.columns = list(range(1,len(sse_df.columns)+1))
+
     
     avg_SRP = df.mean(axis=1)
     sqdev_df = obs.subtract(list(avg_SRP[list(triplet_df["Ot"])]), axis=0)**2
 
+    
     ssd = np.array(list(sqdev_df.sum(axis = 1)))
     sse = np.array(list(sse_df.sum(axis = 1)))
     print("end lack_of_fit")
@@ -181,9 +186,11 @@ def test_triplets(df, triplet_df, lof):
 
 
 def find_network(triplet_df, L):
+    L = float(L)
     print("start find_network")
+    print(triplet_df)
+    print(triplet_df.loc[(triplet_df["Ld"] >= 0),:])
     network_triplets = triplet_df.loc[(triplet_df["Ld"] >= 0) & (triplet_df["Ld"] <= L),:]
-    network_triplets.head()
     network = network_triplets.iloc[:,[0,2]]
     network = network.rename(columns={"Om": "Oc", "Ot": "Ot"})
     network = network.append(network_triplets.iloc[:,[1,2]])
@@ -193,6 +200,7 @@ def find_network(triplet_df, L):
     network["Interaction"] = lm
     network = network.drop_duplicates()
     print("end find_network")
+    print(network)
     return network
 
 # In[18]:
@@ -200,30 +208,31 @@ def find_network(triplet_df, L):
 
 def draw_network(network):
     print("start draw_network")
-    G = nx.from_pandas_edgelist(network, "Oc", "Ot", "Interaction")
-    pos = nx.spring_layout(G)
+    G = nx.from_pandas_edgelist(network, "Oc", "Ot", "Interaction", create_using = nx.DiGraph())
+    pos = nx.circular_layout(G)
 
     nx.draw(G, pos, with_labels=True, font_weight='bold', node_color='#A0CBE2', node_size = 3000, edge_color=network["Interaction"],
         width=4, edge_cmap=plt.cm.Blues)
-    plt.tight_layout()
+    #plt.tight_layout()
     plt.savefig("network.png", format="PNG")
     print("end draw_network")
-
 
 # In[ ]:
 
 
-def main(file, L = 3.8, oligo = False):
-    print("STEP 1")
+def main(file, L = 3.8, oligo = "False"):
     df = format_file(file, oligo)
-    print("STEP 2")
+    print("STEP 1 DONE")
     triplet_df = triplets(df)
-    print("STEP 3")
+    print("STEP 2 DONE")
     lof = lack_of_fit(df, triplet_df)
-    print("STEP 4")
+    print("STEP 3 DONE")
+    triplet_df = test_triplets(df, triplet_df, lof)
+    print("STEP 4 DONE")
     network = find_network(triplet_df, L)
-    print("STEP 5")
+    print("STEP 5 DONE")
     draw_network(network)
+    print("STEP 6 DONE")
 
 
 # In[ ]:
